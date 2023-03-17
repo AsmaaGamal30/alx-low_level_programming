59912ca3d003bad5e1f00367c000c0819d34b6da
Asmaa Gamal <asmagamal311@gmail.com>
2023-03-17 16:12:06 -0700

    5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char a ,q ,e;
+
+	q = 'q';
+	e = 'e';
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		if(a != q && a != e)
+			putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
